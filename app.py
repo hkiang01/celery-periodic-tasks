7b5562075bedde6512a6c9c0cb4fe7580057f0f0
Harrison Kiang <hkiang01@gmail.com>
2019-05-22 22:09:04 -0500

    minimal example for periodic task

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import logging
 from celery import Celery
 
 app = Celery('tasks', broker='redis://redis:6379')
+logger = logging.getLogger(__name__)
 
 @app.task
-def add(x, y):
-    return x + y
+def my_task():
+    logger.info("my_task")
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, my_task.s(), name='test every 10')
